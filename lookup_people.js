c6e06b86727afd59e54473db24a2cc723db4c742
edward eaglesham <edward.eaglesham@gmail.com>
2018-02-06 23:11:31 +0000

    code refactored to printing function, query function and connect function that calls query()

--- lookup_people.js
+++ lookup_people.js
@@ -2,23 +2,37 @@ const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
 });
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query(`SELECT * FROM famous_people WHERE first_name = '${process.argv[2]}' OR last_name = '${process.argv[2]}'`, (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
+const ResultsList = function(result) {
+            
+    for (let i=0; i < result.rowCount; i++) {
+        let bdate = result.rows[i].birthdate.toString().slice(0, 15);
+        console.log(`-${i+1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born '${bdate}'`)
     }
-    console.log(result);
-    client.end();
-  });
+}
+
+const query = function (client, arg) {
+    client.query(`SELECT * FROM famous_people WHERE first_name = '${arg}' OR last_name = '${arg}'`, (err, result) => {
+        if (err) {
+        return console.error("error running query", err);
+        }
+        console.log("searching...");
+        console.log(result)
+        client.end();
+        ResultsList(result);
+    });
+}
+
+client.connect((err) => {
+    if (err) {
+        return console.error("Connection Error", err);
+    }
+    query(client, process.argv[2])
 });
