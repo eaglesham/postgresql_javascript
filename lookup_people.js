586cd8c39dd443361de86473b612c5d74ad3b416
edward eaglesham <edward.eaglesham@gmail.com>
2018-02-06 21:58:24 +0000

    added lookup people js file to allow command line user query of first or last name in database

--- lookup_people.js
+++ lookup_people.js
@@ -14,11 +14,10 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query(`SELECT * FROM famous_people WHERE first_name = '${process.argv[2]}' OR last_name = '${process.argv[2]}'`, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
     console.log(result);
     client.end();
   });
